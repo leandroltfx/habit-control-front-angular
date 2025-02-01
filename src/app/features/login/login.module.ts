3786fa50ed8152f29378dec58137427c742e0f1a
leandroltfx <leandroltfbr@hotmail.com>
2025-02-01 18:30:25 -0300

    criando formulário de login e integrando com serviço

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -13,12 +21,21 @@ import { LoginAdapterService } from './acl/adapter/login-adapter.service';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+
+    LoginRoutingModule,
   ],
   providers: [
     LoginProxyService,
     LoginFacadeService,
     LoginAdapterService,
-  ],
+  ]
 })
 export class LoginModule { }
