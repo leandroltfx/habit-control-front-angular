15700126ecf6b1c22b6b42d83d458a172ca7502e
leandroltfx <leandroltfbr@hotmail.com>
2025-02-07 15:58:16 -0300

    criando módulo para a home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'user-registration', loadChildren: () => import('./features/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
   { path: 'recover-password', loadChildren: () => import('./features/recover-password/recover-password.module').then(m => m.RecoverPasswordModule) },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: '**', redirectTo: 'login' },
 ];
 
